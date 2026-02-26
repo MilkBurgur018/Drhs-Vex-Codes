94126d69039be6a1ca4b2d08f5eafe4b9a3045c8
MilkBurgur018 <supermakoto7@hotmail.com>
2026-02-26 15:36:29 -0800

    haha push because dev needed it ig

--- Main 0.5 A/src/globals.cpp
+++ Main 0.5 A/src/globals.cpp
@@ -1,45 +1,25 @@
 #include "main.h"
+#include "globals.hpp"
 
-// --- INTAKE DEFINITION ---
-// Same for all bots (unless you need to change ports, do it here)
+// Intake / outtake
 pros::Motor intake_motor(7);
 pros::Motor outtake_motor(-8);
-pros::adi::DigitalOut pneumA('A');
-pros::adi::DigitalOut pneumB('B');
-pros::adi::DigitalOut pneumH('H');
-// --- CHASSIS DEFINITIONS ---
 
-// CONFIG 1: COMP BOT - Blue Motors (600), 36:60 Gearing
-
+// EZ-Template chassis
 ez::Drive chassis (
   {-1, -2, -3},
   {4, 5, 6},
-  20,// IMU Port
+  20,
   3.25,
-  600, // Blue Cartridge
-  (36.0 / 60.0) // Keeps the same physical gears
-);
-
-
-
-// CONFIG 2: TEST BOT - Old Ports
-/*
-ez::Drive chassis (
-  // Left: -1, -4, -11
-  {-1, -4, -11},
-  // Right: 3, 5, 13
-  {3, 5, 13},
-  // IMU Port (Change if needed)
-  21,
-  // Wheel Diameter
-  3.25,
-  // Cartridge RPM (Assuming Blue/600 for test bot, change if Green)
   600,
-  // Gear Ratio (Direct drive? Change if needed)
-  1.0
+  (36.0 / 60.0)
 );
-*/
 
-//define motors again in pros for auton and more control
-pros::MotorGroup left_mg({1, -2, -3});
+// Define MotorGroups
+pros::MotorGroup left_mg({-1, -2, 3});
 pros::MotorGroup right_mg({4, 5, 6});
+
+// Pneumatics
+pros::adi::DigitalOut pneumA('A');
+pros::adi::DigitalOut pneumB('B');
+pros::adi::DigitalOut pneumH('H');
--- Main 0.5 A/src/left_qual_auton.cpp
+++ Main 0.5 A/src/left_qual_auton.cpp
@@ -13,23 +13,24 @@ void left_qual_auton() {
     // --- MOVEMENT ---
     // Start intake ONLY when moving toward the first ring
     intake_motor.move_velocity(600); 
+    outtake_motor.move_velocity(500);
 
-    chassis.pid_drive_set(50, 67);
+    chassis.pid_drive_set(42, 67);
     chassis.pid_wait(); 
 
     chassis.pid_turn_set(0, 90);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(100, 67);
+    chassis.pid_drive_set(100, 43);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(-100, 67);
+    chassis.pid_drive_set(-100, 43);
     chassis.pid_wait();
 
     chassis.pid_turn_set(-90, 90);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(55, 67);
+    chassis.pid_drive_set(71, 67);
     chassis.pid_wait();
 
     /*
@@ -54,35 +55,39 @@ void left_qual_auton() {
     chassis.pid_drive_set(24, 67); redundant, no need to double into goal, we have storage
     chassis.pid_wait();
     */
-    chassis.pid_turn_set(180, 90);
+    chassis.pid_turn_set(-180, 90);
     chassis.pid_wait();
 
     pneumA.set_value(true); 
     pros::delay(200);       
 
-    chassis.pid_drive_set(50, 67);
+    chassis.pid_drive_set(37, 67);
     chassis.pid_wait();
 
+    //pre sicko
+    chassis.pid_turn_set(-181, 90);
+    chassis.pid_wait();
+    chassis.drive_brake_set(MOTOR_BRAKE_COAST);
+    pros::delay(20);
     // --- When the robot goes sicko mode ---
     for(int i = 0; i < 3; i++) {
         //using pros native motor calls to shake the robot. bc why the hell not
-        left_mg.move_velocity(100);
-        right_mg.move_velocity(100);
-        pros::delay(600);             
+        chassis.pid_turn_set(-180, 90);
+        pros::delay(300);             
         
-        left_mg.move_velocity(-100);
-        right_mg.move_velocity(-100);
-        pros::delay(600);
+        chassis.pid_turn_set(-181, 90);
+        pros::delay(300);
     }   
+    chassis.drive_brake_set(MOTOR_BRAKE_HOLD);
     // Stop all drivetrain motors
     left_mg.move_velocity(0);
     right_mg.move_velocity(0);
 
     // --- WRAP UP ---
-    chassis.pid_drive_set(100, 67);
+    chassis.pid_drive_set(-100, 67);
     chassis.pid_wait();
 
-    outtake_motor.move_velocity(600); 
+    outtake_motor.move_velocity(-600); 
     pros::delay(2000);
     outtake_motor.move_velocity(0);
 
