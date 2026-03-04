c473ab69fb6336dca1312b4d31192fc7bf29764c
MilkBurgur018 <supermakoto7@hotmail.com>
2026-03-03 16:19:40 -0800

    auton
    very clolse

--- Main 0.5 A/src/left_qual_auton.cpp
+++ Main 0.5 A/src/left_qual_auton.cpp
@@ -15,69 +15,73 @@ void left_qual_auton() {
     intake_motor.move_velocity(600); 
     outtake_motor.move_velocity(500);
 
-    chassis.pid_drive_set(42,77);
+    chassis.pid_drive_set(44, 95);
     chassis.pid_wait(); 
 
-    chassis.pid_turn_set(0, 90);
+    chassis.pid_turn_set(0, 95);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(100, 67);
+    chassis.pid_drive_set(100, 95);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(-100, 67);
+    chassis.pid_drive_set(-100, 95);
     chassis.pid_wait();
 
-    chassis.pid_turn_set(-91, 90);
+    chassis.pid_turn_set(-91, 95);
     chassis.pid_wait();
 
-    chassis.pid_drive_set(71,70);
+    chassis.pid_drive_set(72,95);
     chassis.pid_wait();
 
-    chassis.pid_turn_set(-180, 90);
+    chassis.pid_turn_set(-179, 95);
     chassis.pid_wait();
 
     pneumA.set_value(true); 
-    pros::delay(500);       
+    pros::delay(50);       
     //ram it in
-    chassis.drive_set(80, 80);
-    pros::delay(800);
+    chassis.drive_set(65, 65);
+    pros::delay(995);
     chassis.drive_set(0, 0);
 
     //pre sicko
-    chassis.pid_turn_set(-181, 90);
-    chassis.pid_wait();
     chassis.drive_brake_set(MOTOR_BRAKE_COAST);
     pros::delay(20);
     // --- When the robot goes sicko mode ---
-    for(int i = 0; i < 13; i++) {
+    for(int i = 0; i < 3; i++) {
         //using direct voltage calls to shake the robot. bc why the hell not
         chassis.drive_set(12000, 12000);
-        pros::delay(60);             
-        //
-        chassis.drive_set(-12000, -12000);  
-        pros::delay(60);       
+        pros::delay(90);      
+        chassis.drive_set(0,0);      
+        pros::delay(110); 
+        chassis.drive_set(-10000, -10000);  
+        pros::delay(70); 
+        chassis.drive_set(0,0);
+        pros::delay(110);      
     }   
+    chassis.drive_set(12000, 12000);
+    pros::delay(600);      
+    chassis.drive_set(0,0);
     chassis.drive_brake_set(MOTOR_BRAKE_HOLD);
-    // Stop all drivetrain motors
-    left_mg.move_velocity(0);
-    right_mg.move_velocity(0);
 
     //wrap it up
-    chassis.pid_drive_set(-80,70);
+    chassis.pid_turn_set(-181, 127);
     chassis.pid_wait();
 
-    outtake_motor.move_velocity(-12000); 
-    pros::delay(4000);
-    outtake_motor.move_velocity(0);
+    chassis.pid_drive_set(-80,127);
+    chassis.pid_wait_until(-73);
 
-    pneumA.set_value(false); 
+    outtake_motor.move_velocity(-12000); 
+    pros::delay(2600);
+    outtake_motor.move_velocity(0);
 
     //Ram
     chassis.drive_set(40, 40);
-    pros::delay(900); 
+    pros::delay(950); 
     chassis.drive_set(0, 0);
 
     chassis.drive_set(-12000, -12000);
     pros::delay(700);
     chassis.drive_set(0, 0);
+
+    pneumA.set_value(false); 
 }
